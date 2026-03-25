e43f38b7b91be81e04470296d7933ec2abab1885
Fahim Ahmed <ahmedfahimwork@gmail.com>
2026-03-25 20:08:25 +0530

    examples for simulating long running tasks

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,7 +3,11 @@ import { inngest } from "./client";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world", triggers: [{ event: "test/hello.world" }] },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
+    // Simulate a long-running task by sleeping for 20 seconds
+    await step.sleep("wait-a-moment", "20s");
+
+    // Simulate a long-running task by sleeping for 30 seconds
+    await step.sleep("wait-a-moment", "30s");
     return { message: `Hello ${event.data.email}!` };
   },
 );
